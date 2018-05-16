fcff7dcd447be8e80aadd5c620f2706014c5db22
pratajski <p.ratajski@gmail.com>
2018-05-16 18:55:22 +0200

    7,5 drugie podejscie

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,7 +1,14 @@
 package com.kodilla.stream.array;
 
+import java.util.stream.IntStream;
+
 public interface ArrayOperations {
-    static void getAverage(int[] numbers){
+    static double getAverage(int[] numbers){
+
+        return IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average()
+                .getAsDouble();
 
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/array/Average.java
+++
@@ -1,6 +0,0 @@
-package com.kodilla.stream.array;
-
-public class Average {
-
-
-}
