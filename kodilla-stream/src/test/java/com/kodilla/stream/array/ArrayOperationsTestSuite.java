c21ba0bde9a1ffafb14e49acc89c72b54835ee35
pratajski <p.ratajski@gmail.com>
2018-05-17 08:21:29 +0200

    7,5 czwartek

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,11 +1,17 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage() {
         //Given
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+        //when
+        double average = ArrayOperations.getAverage(numbers);
+        //Then
+        Assert.assertEquals(9.0, average, 0);
 
     }
 }
