0c0f8c7840aecb07964d33bf614b04581d677807
pratajski <p.ratajski@gmail.com>
2018-05-31 21:08:07 +0200

    8,3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            secondChallenge.probablyIWillThrowException(2,1);
+        }catch (Exception e)
+        {
+            System.out.println("Wrong parametr");
+        }finally {
+            System.out.println("Exception checked");
+        }
+
+    }
+
+
+
+
+
+
+
+}
