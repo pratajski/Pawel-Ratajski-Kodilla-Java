eb176ee595a278aa80f553b69ee1a74b87f56196
pratajski <p.ratajski@gmail.com>
2018-06-09 09:14:45 +0200

    9,1 wstep

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamRunner.java
@@ -0,0 +1,16 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.List;
+import java.util.Map;
+
+public class StreamRunner {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+
+     //
+
+
+
+    }
+}
