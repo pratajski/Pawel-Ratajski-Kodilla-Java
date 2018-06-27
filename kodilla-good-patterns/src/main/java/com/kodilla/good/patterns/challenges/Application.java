8074b0f175f6dd1d39c4949537207e14a0d800c3
pratajski <p.ratajski@gmail.com>
2018-06-27 19:14:37 +0200

    wstep do 9,2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Application.java
@@ -9,8 +9,8 @@ public class Application {
         LocalDateTime rentFrom = LocalDateTime.of(2017, 8, 1 , 12, 0);
         LocalDateTime rentTo = LocalDateTime.of(2017, 8, 10 , 12, 0);
 
-        RentalProcessor rentalProcessor = new RentalProcessor(new MailService(), new CarRentalService(), new CarRentalRepository());
-        rentalProcessor.process(user, rentFrom, rentTo);
+       // RentalProcessor rentalProcessor = new RentalProcessor(new MailService(), new CarRentalService(), new CarRentalRepository());
+     //   rentalProcessor.process(user, rentFrom, rentTo);
 
 
     }
