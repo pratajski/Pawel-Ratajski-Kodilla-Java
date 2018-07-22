bb9c7a1ebf5eb51c068c85e8c3238d24188c61c2
pratajski <p.ratajski@gmail.com>
2018-07-22 13:59:26 +0200

    11,2 wstęp

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,14 @@
+package com.kodilla.patterns.prototype.library;
+
+import org.junit.Test;
+
+public class LibraryTestSuite {
+    @Test
+    public void testGetBooks() {
+    //Given
+
+    // When
+
+    //Then
+    }
+}
