92a2ef9cf7eb58c322d31df1c9545ae71c8ca0ed
pratajski <p.ratajski@gmail.com>
2018-08-03 21:08:10 +0200

    13,2

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Test
+    public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(DESCRIPTION, 7);
+        taskListDao.save(taskList);
+        int duration = taskList.getId();
+
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName();
+
+        //Then
+        Assert.assertEquals(1, readTaskList.size());
+
+        //CleanUp
+        int id = readTaskList.get(0).getId();
+        taskListDao.delete(id);
+    }
+}
