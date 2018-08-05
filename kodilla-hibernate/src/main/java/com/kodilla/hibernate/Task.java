e0b98ada6084bf4d9a47c2b8be1c0cea4bd082bc
pratajski <p.ratajski@gmail.com>
2018-08-05 14:04:43 +0200

    13,3 cd

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/Task.java
@@ -1,6 +1,7 @@
 package com.kodilla.hibernate;
 
 import com.kodilla.hibernate.task.TaskFinancialDetails;
+import com.kodilla.hibernate.tasklist.TaskList;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -14,6 +15,7 @@ public final class Task {
     private Date created;
     private int duration;
     private TaskFinancialDetails taskFinancialDetails;
+    private TaskList taskList;
 
     public Task() {
     }
@@ -50,6 +52,16 @@ public final class Task {
 
     public TaskFinancialDetails getTaskFinancialDetails() {
         return taskFinancialDetails;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "TASKLIST_ID")
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(TaskList taskList) {
+        this.taskList = taskList;
     }
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
@@ -0,0 +1,49 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Table(name = "INVOICE")
+public class Invoice {
+    private int id;
+    private String number;
+    private List<Item> items;
+
+    public Invoice(int id, String number, List<Item> items) {
+        this.id = id;
+        this.number = number;
+        this.items = items;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "INVOICE_ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @NotNull
+    @Column(name = "INVOICE_NUMBER")
+    public String getNumber() {
+        return number;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
